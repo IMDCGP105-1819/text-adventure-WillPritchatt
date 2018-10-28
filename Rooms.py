a56fcb12c8f1c994f768ad1825d98a38125ea4a1
Will Pritchatt <Will.pritchatt@btopenworld.com>
2018-10-28 23:37:00 +0000

    Rooms Script added direction checker basis

--- Rooms.py
+++ Rooms.py
@@ -1,5 +1,12 @@
 class Room:
     def __init__(self, name, directions, interactables):
-        room_name = name
-        room_directions = directions
-        room_items = interactables
+        self.room_name = name
+        self.room_directions = directions
+        self.room_items = interactables
+
+    def check_direction(self, input_direction):
+        for i in self.room_directions:
+            if i in input_direction:
+                return True
+            else:
+                return False
